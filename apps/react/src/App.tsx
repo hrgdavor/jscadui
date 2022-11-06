c2496fd7cb3d94b6665632eee06f6f5cfe4409a1
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-11-06 13:14:56 +0100

    first-commit

---
+++ apps/react/src/App.tsx
@@ -0,0 +1,12 @@
+import { light } from '@jscadui/themes'
+
+function App() {
+  console.log(light)
+  return (
+    <div className="App">
+      <h1>jscad ui</h1>
+    </div>
+  )
+}
+
+export default App

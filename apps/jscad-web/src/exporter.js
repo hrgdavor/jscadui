50163bdfc3b72fca4a563b36a23c127aac8b74ab
Kenny Daniel <platypii@gmail.com>
2023-07-30 10:13:10 -0700

    jscad-web: populate export formats from code

--- apps/jscad-web/src/exporter.js
+++ apps/jscad-web/src/exporter.js
@@ -1,4 +1,18 @@
+
+const exportFormats = [
+  { name: "stla", extension: "stl", label: "STL (ascii)" },
+  { name: "stlb", extension: "stl", label: "STL (binary)" },
+  { name: "amf", extension: "amf", label: "AMF" },
+  { name: "dxf", extension: "dxf", label: "DXF" },
+  { name: "json", extension: "json", label: "JSON" },
+  { name: "obj", extension: "obj", label: "OBJ" },
+  { name: "x3d", extension: "x3d", label: "X3D" },
+  { name: "svg", extension: "svg", label: "SVG" },
+  { name: "3mf", extension: "3mf", label: "3MF" },
+]
+
 export const init = (exportFn) => {
+  populateFormats(exportFormats)
   // Bind export buttons
   document.getElementById("export-button").addEventListener("click", () => {
     // Export model in selected format
@@ -6,6 +20,14 @@ export const init = (exportFn) => {
     const format = formatSelect.value
     exportFn(format)
   })
+}
 
-  // TODO: Populate formats from @jscad/io
+const populateFormats = (formats) => {
+  const select = document.getElementById("export-format")
+  formats.forEach((format) => {
+    const option = document.createElement("option")
+    option.value = format.name
+    option.text = format.label
+    select.appendChild(option)
+  })
 }

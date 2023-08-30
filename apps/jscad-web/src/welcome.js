362c1f9d40d6d1e3755b2ec640d690818fcc9ca0
Kenny <platypii@gmail.com>
2023-08-30 02:17:48 -0700

    jscad-web: fix default model (#50)
    1. there was an issue where if you either load a model via remote url,
    or if you're quick about drag-and-dropping a file, the default model
    will load AFTER it, and replace the content. This PR fixes that by
    disabling default model loading once any other model has been loaded
    2. add `robots.txt` file (allow all)
    3. cleanup the `serve.js` code style
    4. tweak hull example to show how it can be used to make a shape that
    **isn't** just a rounded rectangle
    5. easter egg: add a greeting message to any users who open the dev
    console. `Welcome to JSCAD! Like JavaScript and want to help? Join us at
    https://github.com/jscad/OpenJSCAD.org`

--- apps/jscad-web/src/welcome.js
+++ apps/jscad-web/src/welcome.js
@@ -1,13 +1,15 @@
 const welcome = document.getElementById("welcome")
 let showing = true
 
-// Hide the welcome menu when anything is clicked
 export const init = () => {
+  // hide the welcome menu when anything is clicked
   window.addEventListener("mousedown", (e) => dismiss(e))
   window.addEventListener("click", (e) => dismiss(e))
   window.addEventListener("drop", () => dismiss())
   window.addEventListener("dragstart", () => dismiss())
   window.addEventListener("dragover", () => dismiss())
+  // hello devs
+  console.log('Welcome to JSCAD! Like JavaScript and want to help? Join us at https://github.com/jscad/OpenJSCAD.org')
 }
 
 export const dismiss = (e) => {

hrgdavor/jscadui

commit 1fef74f07a81af1f4d14f1a34a5a999d36f58217

advanced require (#51)
update require function to read files from modules, not just bundled
modules on jsdelivr.

---------

Co-authored-by: Kenny Daniel <platypii@gmail.com>

apps/jscad-web/main.js
@@ -152,6 +152,7 @@ async function sendCmdAndSpin(method, params) {
 sendCmdAndSpin('init', {
   bundles: {
     '@jscad/modeling': toUrl('./build/bundle.jscad_modeling.js'),
+    '@jscad/io': toUrl('./build/bundle.jscad_io.js'),
   },
 }).then(() => {
   if (loadDefault) {

packages/require/src/require.js
@@ -10,6 +10,11 @@
 - typescript import must use .js (it is a bit strange, but probably has good reasons)
 */
 
+import { readFileWeb } from './readFileWeb'
+import { MODULE_BASE, getExtension, resolveUrl } from './resolveUrl'
+
+export { resolveUrl } from './resolveUrl'
+
 // initially new Function was used to pass parameters: require, exports, module
 // new Functions screws with sourcemaps as it adds a prefix to the source
 // we need eval to do the same without prefix
@@ -20,62 +25,7 @@ export const runModule = (typeof self === 'undefined' ? eval : self.eval)(
   '(require, exports, module, source)=>eval(source)',
 )
 
-const MODULE_BASE = 'https://cdn.jsdelivr.net/npm/'
-
-export const selfish = (func, context) => {
-  const out = func.bind({ context })
-  out.bind = newContext => selfish(func, newContext)
-  out.func = func
-  out.context = context
-  return out
-}
-
-/**
- * Resolve a package or file name to a url.
- * JS packages will resolve to an npm package url.
- * Relative urls will resolve to a local url.
- * @param {string} url the package or file name to resolve
- * @param {string} base the url of the current module to resolve relative to
- * @param {string} root the url under which local files are served
- * @param {string} moduleBase the url for npm packages
- * @returns the resolved module url
- */
-export const resolveUrl = (url, base, root, moduleBase=MODULE_BASE)=>{
-  let isRelativeFile = false
-  let isModule = false
-  let cacheUrl = url
-  
-  if (!/^(http:|https:|fs:|file:)/.test(url)) {
-    // npm modules cannot start with . or /
-    if (!/^\.?\.?\//.test(url)) {
-      isModule = true
-      url = new URL(url, moduleBase).toString()
-    } else {
-      isRelativeFile = true
-      // sanitize to avoid going below root, it will prevent / to go below cache baseUrl
-      // it will prevent ../../../../ to go below cache baseUrl
-      const fromRoot = root && url[0] === '/'
-      if (!fromRoot) {
-        // base relative path
-        const relativePath = base.replace(/^\//, '').replace(root, '') // strip root
-        // create url relative path
-        url = new URL(url, `fs:/root/${relativePath}`).toString()
-        // check if url went above root
-        if (!url.startsWith('fs:/root/')) throw new Error('relative url cannot go above root')
-        url = url.substring(9)
-      } else {
-        url = url.substring(1)
-      }
-      cacheUrl = `/${url}`
-      // now create the full url to load the file
-      url = new URL(url, root).toString()
-    }
-  }
-  
-  return { url, isRelativeFile, isModule, cacheUrl }
-}
-
-export function require(urlOrSource, transform, _readFile, _base, root, readModule, moduleBase = MODULE_BASE) {
+export function require(urlOrSource, transform, _readFile=readFileWeb, _base, root, readModule, moduleBase = MODULE_BASE) {
   let source
   let url
   let isRelativeFile
@@ -107,14 +57,27 @@ export function require(urlOrSource, transform, _readFile, _base, root, readModu
     cacheUrl = resolved.cacheUrl
     isRelativeFile = resolved.isRelativeFile
 
-    if(isModule) readFile = readModule
-    base = url
+    if (isModule) {
+      readFile = readModule
+      base = root = url + '/'
+    }
+
     cache = requireCache[isRelativeFile ? 'local':'module']
     exports = cache[cacheUrl] // get from cache
     if (!exports) {
       // not cached
       try {
         source = readFile(url, { base })
+        if (url.includes('jsdelivr.net')) {
+          // jsdelivr will read package.json and tell us what the main file is
+          const srch = ' * Original file: '
+          let idx = source.indexOf(srch)
+          if (idx != -1) {
+            let idx2 = source.indexOf('\n', idx+srch.length+1)
+            let realFile = new URL(source.substring(idx+srch.length, idx2), url).toString()
+            url = base = realFile
+          }
+        }
       } catch (e) {
         if (url.endsWith('.js')) {
           try {
@@ -131,16 +94,18 @@ export function require(urlOrSource, transform, _readFile, _base, root, readModu
     }
   }
   if (source !== undefined) {
-    // do not transform bundles that are already cjs ( requireCache.bundleAlias.*)
-    if (transform && !bundleAlias) source = transform(source, url).code
-    let requireFunc = newUrl => require(newUrl, transform, readFile, url, root, readModule, moduleBase)
-    const module = requireModule(url, source, requireFunc)
-    module.local = isRelativeFile
-    exports = module.exports
-  }
-
-  if (typeof exports !== 'object') {
-    throw new Error(`module ${cacheUrl} did not export an object`)
+    let extension = getExtension(url)
+    // https://cdn.jsdelivr.net/npm/@jscad/svg-serializer@2.3.13/index.js uses require to read package.json
+    if (extension === 'json') {
+      exports = JSON.parse(source)
+    } else {
+      // do not transform bundles that are already cjs ( requireCache.bundleAlias.*)
+      if (transform && !bundleAlias) source = transform(source, url).code
+      let requireFunc = newUrl => require(newUrl, transform, readFile, base, root, readModule, moduleBase)
+      const module = requireModule(url, source, requireFunc)
+      module.local = isRelativeFile
+      exports = module.exports
+    }
   }
 
   if(cache) cache[cacheUrl] = exports // cache obj exported by module
@@ -168,7 +133,6 @@ export function requireModule(url, source, _require) {
 export const clearFileCache = async ({files}) => {
   const cache = requireCache.local
   files.forEach(f=>{
-    console.warn('clear', f, cache[f])
     delete cache[f]
   })
 }

packages/require/src/resolveUrl.js
@@ -1,0 +1,76 @@
+
+export const MODULE_BASE = 'https://cdn.jsdelivr.net/npm/'
+
+export const getExtension = (url) => {
+  let arr = url.split('/')
+  let file = arr[arr.length-1]
+  let idx = file.lastIndexOf('.')
+  return idx == -1 ? '' : file.substring(idx+1)
+}
+
+const splitModuleName = (module) => {
+  let file = ''
+  let idx = module.indexOf('/')
+  if (module[0] === '@') idx = module.indexOf('/',idx+1)
+  if (idx !== -1) {
+    file = module.substring(idx+1)
+    module = module.substring(0,idx)
+  }
+  return [module,file]
+}
+
+/**
+ * Resolve a package or file name to a url.
+ * JS packages will resolve to an npm package url.
+ * Relative urls will resolve to a local url.
+ * @param {string} url the package or file name to resolve
+ * @param {string} base the url of the current module to resolve relative to
+ * @param {string} root the url under which local files are served
+ * @param {string} moduleBase the url for npm packages
+ * @returns the resolved module url
+ */
+export const resolveUrl = (url, base, root, moduleBase=MODULE_BASE) => {
+  let isRelativeFile = false
+  let isModule = false
+  let cacheUrl = url
+  
+  if (!/^(http:|https:|fs:|file:)/.test(url)) {
+    // npm modules cannot start with . or /
+    if (!/^\.?\.?\//.test(url)) {
+      const [moduleName, moduleFile] = splitModuleName(url)
+      const moduleUrl = new URL(moduleName, moduleBase).toString()
+      if (moduleFile) {
+        base = root = moduleUrl + '/'
+        url = moduleFile
+        isRelativeFile = true
+      } else {
+        isModule = true
+        url = moduleUrl
+      }
+    } else {
+      isRelativeFile = true
+    }
+
+    if (isRelativeFile && root) {
+      // sanitize to avoid going below root, it will prevent / to go below cache baseUrl
+      // it will prevent ../../../../ to go below cache baseUrl
+      const fromRoot = root && url[0] === '/'
+      if (!fromRoot) {
+        // base relative path
+        const relativePath = base.replace(/^\//, '').replace(root, '') // strip root
+        // create url relative path
+        url = new URL(url, `fs:/root/${relativePath}`).toString()
+        // check if url went above root
+        if (!url.startsWith('fs:/root/')) throw new Error('relative url cannot go above root')
+        url = url.substring(9)
+      } else {
+        url = url.substring(1)
+      }
+      if (!getExtension(url)) url += '.js'
+      cacheUrl = `/${url}`
+      // now create the full url to load the file
+      url = new URL(url, root).toString()
+    }
+  }
+  return { url, isRelativeFile, isModule, cacheUrl }
+}

packages/require/test/resolveUrl.test.js
@@ -1,0 +1,27 @@
+import { expect, it } from 'vitest'
+
+import { resolveUrl } from '../src/resolveUrl.js'
+
+const localBase = 'http://localhost:5120/swfs/5810d898-ed92-4d48-b22f-1b7d6353117a/'
+const remoteBase = 'https://cdn.jsdelivr.net/npm/@jscad/io-utils/'
+const root = 'http://localhost:5120/swfs/5810d898-ed92-4d48-b22f-1b7d6353117a/'
+
+it('local file', () => {
+  const expected = 'http://localhost:5120/swfs/5810d898-ed92-4d48-b22f-1b7d6353117a/imported.js'
+  expect(resolveUrl('./imported.js', '/index.js', root).url).toEqual(expected)
+  expect(resolveUrl('./imported.js', localBase, root).url).toEqual(expected)
+})
+
+it('npm package', () => {
+  expect(resolveUrl('@org/package', localBase, root).url).toEqual('https://cdn.jsdelivr.net/npm/@org/package')
+  expect(resolveUrl('toppackage', remoteBase, root).url).toEqual('https://cdn.jsdelivr.net/npm/toppackage')
+})
+
+it('npm package internal file', () => {
+  expect(resolveUrl('@jscad/io-utils/convertToBlob', localBase, root).url).toEqual('https://cdn.jsdelivr.net/npm/@jscad/io-utils/convertToBlob.js')
+})
+
+it('npm package relative', () => {
+  expect(resolveUrl('./convertToBlob.js', remoteBase, root).url)
+    .toEqual('https://cdn.jsdelivr.net/npm/@jscad/io-utils/convertToBlob.js')
+})

apps/engine-test/build.js
@@ -21,7 +21,7 @@ await buildBundle(outDir + '/build', 'bundle.regl.js', { globalName: 'REGL' })
 await buildBundle(outDir + '/build', 'bundle.babylonjs.js', { globalName: 'BABYLON' })
 await buildBundle(outDir + '/build', 'bundle.threejs.js', { globalName: 'THREE' })
 await buildBundle(outDir + '/build', 'bundle.jscad_modeling.js', { format: 'cjs' })
-await buildBundle(outDir + '/build', 'bundle.jscad.io.js', { globalName: 'jscad_io' })
+await buildBundle(outDir + '/build', 'bundle.jscad_io.js', { globalName: 'jscad_io' })
 await buildBundle(outDir + '/build', 'bundle.jscadui.transform-babel.js', { globalName: 'jscadui_transform_babel' })
 
 /**************************** BUILD JS THAT can change and watch if in dev mode *************/

apps/engine-test/src_bundle/bundle.worker.js
@@ -9,7 +9,7 @@ import {currentSolids, initWorker} from '@jscadui/worker'
 import { withTransferable } from '@jscadui/postmessage'
 
 const exportData = ({format})=>{
-  if(typeof jscad_io === 'undefined') importScripts('./bundle.jscad.io.js')
+  if(typeof jscad_io === 'undefined') importScripts('./bundle.jscad_io.js')
   const solids = currentSolids()
   console.log('exportData format', format, jscad_io)
   const data = jscad_io.stlSerializer.serialize({binary:false}, solids)

commit e60c6e65337883fbfd09ca83de993c3b7815701b

Fix require edge cases (#54)
Very small PR that fixes two issues:

1. Fix `clearCache` function which was doing nothing, due to misuse of
`Cache.keys()` function.

2. Fix regression on relative imports. For example, import
`./brother.js` from `src/sister.js` was missing `src` from base path,
and so it would look in the top level directory instead of the `src`
folder. This worked previously but was regressed by #51.

`index.js`:
```js
const bro = require('./src/brother.js')
```

`src/brother.js`:
```js
const sis = require('./sister.js')
```

`src/sister.js`:
```js
module.exports = { main: () => [] }
```

---------

Co-authored-by: Davor Hrg <hrgdavor@gmail.com>

apps/jscad-web/main.js
@@ -74,7 +74,7 @@ document.body.ondrop = async ev => {
     sendCmd('clearTempCache', {})
     const { alias, script } = await fileDropped(sw, files)
     projectName = sw.projectName
-    if (alias?.length) {
+    if (alias.length) {
       sendNotify('init', { alias })
     }
     runScript({ url: sw.fileToRun, base: sw.base })
@@ -151,7 +151,7 @@ async function sendCmdAndSpin(method, params) {
 }
 
 sendCmdAndSpin('init', {
-  bundles: {
+  bundles: {// local bundled alias for common libs.
     '@jscad/modeling': toUrl('./build/bundle.jscad_modeling.js'),
     '@jscad/io': toUrl('./build/bundle.jscad_io.js'),
   },

packages/fs-provider/fs-provider.js
@@ -129,7 +129,7 @@ export const clearFs = async sw => {
 }
 
 export const clearCache = async cache => {
-  await cache.keys(key => cache.delete(key))
+  (await cache.keys()).forEach(key => cache.delete(key))
 }
 
 export const extractEntries = dt => {
@@ -228,26 +228,7 @@ export async function fileDropped(sw, files) {
   rootFiles = rootFiles.map(e => fileToFsEntry(e, '/'))
   sw.roots.push(rootFiles)
 
-  const alias = []
-  let pkgFile = await findFileInRoots(sw.roots, 'package.json')
-  if (pkgFile) {
-    try {
-      const pack = JSON.parse(await readAsText(pkgFile))
-      if (pack.main) sw.fileToRun = pack.main
-      if (pack.workspaces)
-        for (let i = 0; i < pack.workspaces.length; i++) {
-          const w = pack.workspaces[i]
-          // debugger
-          let pack2 = await findFileInRoots(sw.roots, `/${w}/package.json`)
-          if (pack2) pack2 = JSON.parse(await readAsText(pack2))
-          let name = pack2?.name || w
-          let main = pack2?.main || 'index.js'
-          alias.push([`/${w}/${main}`, name])
-        }
-    } catch (error) {
-      console.error('error parsing package.json', error)
-    }
-  }
+  const alias = await getWorkspaceAliases(sw)
 
   let time = Date.now()
   const preLoad = ['/' + sw.fileToRun, '/package.json']
@@ -270,6 +251,37 @@ export async function fileDropped(sw, files) {
     }
   }
   return {alias, script}
+}
+
+/**
+ * Parse package.json, and return a list of workspace aliases.
+ * Also parses the main file to run, if any.
+ *
+ * @param {SwHandler} sw
+ * @returns {Array}
+ */
+const getWorkspaceAliases = async (sw) => {
+  const alias = []
+  let pkgFile = await findFileInRoots(sw.roots, 'package.json')
+  if (pkgFile) {
+    try {
+      const pack = JSON.parse(await readAsText(pkgFile))
+      if (pack.main) sw.fileToRun = pack.main
+      if (pack.workspaces)
+        for (let i = 0; i < pack.workspaces.length; i++) {
+          const w = pack.workspaces[i]
+          // debugger
+          let pack2 = await findFileInRoots(sw.roots, `/${w}/package.json`)
+          if (pack2) pack2 = JSON.parse(await readAsText(pack2))
+          let name = pack2?.name || w
+          let main = pack2?.main || 'index.js'
+          alias.push({ name, path: `/${w}/${main}` })
+        }
+    } catch (error) {
+      throw new Error(`failed to parse package.json\n  ${error}`)
+    }
+  }
+  return alias
 }
 
 export const findByFsPath = (arr, file) => {

packages/postmessage/index.js
@@ -74,10 +74,27 @@ const messageSender = _self => {
       throw error
     }
   }
+
+  /**
+   * Send a message with no response
+   *
+   * @param {string} method
+   * @param {object} params
+   * @param {Array} trans
+   */
   const sendNotify = (method, params = {}, trans = []) => {
     _self.postMessage({ method, params }, fixTransfer(trans))
   }
 
+  /**
+   * Send a message with response expected
+   *
+   * @param {string} method
+   * @param {object} params
+   * @param {Array} trans
+   * @param {number?} timeout
+   * @returns {Promise} resolves when response is received
+   */
   const sendCmd = (method, params = {}, trans = [], timeout) => {
     const id = seq++
     _self.postMessage({ method, params, id }, fixTransfer(trans))
@@ -98,7 +115,7 @@ const messageSender = _self => {
 
 /**
  *
- * @param {*} _self reference to self of the main widnow (self) or reference to a worker
+ * @param {*} _self reference to self of the main window (self) or reference to a worker
  * @param {*} handlers - object where key if method name, and value ih handler
  * @returns
  */

packages/require/src/readFileWeb.js
@@ -1,7 +1,7 @@
 // it is possible to read binary data
 // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Sending_and_Receiving_Binary_Data
 
-export const readFileWeb = (path, {base = '', output='text'})=>{
+export const readFileWeb = (path, {base = '', output='text'}={}) => {
   const req = new XMLHttpRequest()
   req.open('GET', base ? new URL(path, base) : path, 0) // sync
   req.send()

packages/require/src/require.js
@@ -10,7 +10,6 @@
 - typescript import must use .js (it is a bit strange, but probably has good reasons)
 */
 
-import { readFileWeb } from './readFileWeb'
 import { MODULE_BASE, getExtension, resolveUrl } from './resolveUrl'
 
 export { resolveUrl } from './resolveUrl'
@@ -25,11 +24,10 @@ export const runModule = (typeof self === 'undefined' ? eval : self.eval)(
   '(require, exports, module, source)=>eval(source)',
 )
 
-export function require(urlOrSource, transform, _readFile=readFileWeb, _base, root, readModule, moduleBase = MODULE_BASE) {
+export const require = (urlOrSource, transform, readFile, base, root, moduleBase = MODULE_BASE) => {
   let source
   let url
   let isRelativeFile
-  let _url
   let cache
   let cacheUrl
   let bundleAlias
@@ -40,75 +38,66 @@ export function require(urlOrSource, transform, _readFile=readFileWeb, _base, ro
     url = urlOrSource.url
     isRelativeFile = true
   }
-  let readFile = _readFile
-  let base = _base
   let exports
-  
+  let resolvedUrl = url
+
   if (source === undefined) {
     bundleAlias = requireCache.bundleAlias[url]
-    _url = requireCache.alias[url] || url
-    cacheUrl = _url
-    
-    if(bundleAlias) _url = bundleAlias
-  
-    let resolved = resolveUrl(_url, base, root, moduleBase)
-    const { isModule } = resolved
-    url = resolved.url
-    cacheUrl = resolved.cacheUrl
-    isRelativeFile = resolved.isRelativeFile
+    const aliasedUrl = bundleAlias || requireCache.alias[url] || url
 
-    if (isModule) {
-      readFile = readModule
-      base = root = url + '/'
-    }
+    const resolved = resolveUrl(aliasedUrl, base, root, moduleBase)
+    isRelativeFile = resolved.isRelativeFile
+    resolvedUrl = resolved.url
+    cacheUrl = resolved.url
 
     cache = requireCache[isRelativeFile ? 'local':'module']
     exports = cache[cacheUrl] // get from cache
     if (!exports) {
       // not cached
       try {
-        source = readFile(url, { base })
-        if (url.includes('jsdelivr.net')) {
+        source = readFile(resolvedUrl)
+        if (resolvedUrl.includes('jsdelivr.net')) {
           // jsdelivr will read package.json and tell us what the main file is
           const srch = ' * Original file: '
           let idx = source.indexOf(srch)
           if (idx != -1) {
-            let idx2 = source.indexOf('\n', idx+srch.length+1)
-            let realFile = new URL(source.substring(idx+srch.length, idx2), url).toString()
-            url = base = realFile
+            const idx2 = source.indexOf('\n', idx+srch.length+1)
+            const realFile = new URL(source.substring(idx+srch.length, idx2), resolvedUrl).toString()
+            resolvedUrl = base = realFile
           }
         }
       } catch (e) {
-        if (url.endsWith('.js')) {
+        if (resolvedUrl.endsWith('.js')) {
           try {
-            url = url.substring(0, url.length - 2) + 'ts'
-            source = readFile(url, { base })
+            resolvedUrl = resolvedUrl.replace(/\.js$/, '.ts')
+            source = readFile(resolvedUrl)
           } catch (e2) {
             console.error('failed to load fallback .ts')
-            throw e
+            throw new Error(`failed to load module ${url}\n  ${e}`)
           }
-        }else{
-          throw e
+        } else {
+          throw new Error(`failed to load module ${url}\n  ${e}`)
         }
       }
     }
   }
   if (source !== undefined) {
-    let extension = getExtension(url)
+    let extension = getExtension(resolvedUrl)
     // https://cdn.jsdelivr.net/npm/@jscad/svg-serializer@2.3.13/index.js uses require to read package.json
     if (extension === 'json') {
       exports = JSON.parse(source)
     } else {
       // do not transform bundles that are already cjs ( requireCache.bundleAlias.*)
-      if (transform && !bundleAlias) source = transform(source, url).code
-      let requireFunc = newUrl => require(newUrl, transform, readFile, base, root, readModule, moduleBase)
-      const module = requireModule(url, source, requireFunc)
+      if (transform && !bundleAlias) source = transform(source, resolvedUrl).code
+      // construct require function relative to resolvedUrl
+      let requireFunc = newUrl => require(newUrl, transform, readFile, resolvedUrl, root, moduleBase)
+      const module = requireModule(url, resolvedUrl, source, requireFunc)
       module.local = isRelativeFile
       exports = module.exports
     }
   }
 
-  if(cache) cache[cacheUrl] = exports // cache obj exported by module
+  if (cache && cacheUrl) cache[cacheUrl] = exports // cache obj exported by module
   // TODO research maybe in the future, why going through babel adds __esModule=true
   // this extra reference via defaults helps
   // exports.__esModule = false // this did not help
@@ -117,19 +106,22 @@ export function require(urlOrSource, transform, _readFile=readFileWeb, _base, ro
   return exports // require returns object exported by module
 }
 
-export function requireModule(url, source, _require) {
+const requireModule = (id, url, source, _require) => {
   try {
     const exports = {}
-    const module = { id: url, uri: url, exports: exports, source } // according to node.js modules
+    const module = { id, uri: url, exports, source } // according to node.js modules
     //module.require = _require
     runModule(_require, exports, module, source)
     return module
   } catch (err) {
     console.error('error loading module ' + url, err)
-    throw err
+    throw new Error(`failed loading module ${id}\n  ${err}`)
   }
 }
 
+/**
+ * Clear file cache for specific files. Used when a file has changed.
+ */
 export const clearFileCache = async ({files}) => {
   const cache = requireCache.local
   files.forEach(f=>{
@@ -137,7 +129,10 @@ export const clearFileCache = async ({files}) => {
   })
 }
 
-export const clearTempCache = async ({}) => {
+/**
+ * Clear project-specific cache
+ */
+export const clearTempCache = () => {
   requireCache.local = {}
   requireCache.alias = {}
 }

packages/require/src/resolveUrl.js
@@ -32,7 +32,6 @@ const splitModuleName = (module) => {
 export const resolveUrl = (url, base, root, moduleBase=MODULE_BASE) => {
   let isRelativeFile = false
   let isModule = false
-  let cacheUrl = url
   
   if (!/^(http:|https:|fs:|file:)/.test(url)) {
     // npm modules cannot start with . or /
@@ -67,10 +66,9 @@ export const resolveUrl = (url, base, root, moduleBase=MODULE_BASE) => {
         url = url.substring(1)
       }
       if (!getExtension(url)) url += '.js'
-      cacheUrl = `/${url}`
       // now create the full url to load the file
       url = new URL(url, root).toString()
     }
   }
-  return { url, isRelativeFile, isModule, cacheUrl }
+  return { url, isRelativeFile, isModule }
 }

packages/worker/worker.js
@@ -1,6 +1,6 @@
 import { JscadToCommon } from '@jscadui/format-jscad'
 import { initMessaging, withTransferable } from '@jscadui/postmessage'
-import { clearFileCache, clearTempCache, readFileWeb, require, requireCache, requireModule, resolveUrl } from '@jscadui/require'
+import { clearFileCache, clearTempCache, readFileWeb, require, requireCache, resolveUrl } from '@jscadui/require'
 
 import { exportStlText } from './exportStlText.js'
 import { combineParameterDefinitions, getParameterDefinitionsFromSource } from './getParameterDefinitionsFromSource.js'
@@ -31,14 +31,9 @@ export const init = params => {
   if (baseURI) globalBase = baseURI
 
   if (bundles) Object.assign(requireCache.bundleAlias, bundles)
-  alias?.forEach(arr => {
-    const [orig, ...aliases] = arr
-    aliases.forEach(a => {
-      requireCache.alias[a] = orig
-      if (a.toLowerCase().substr(-3) !== '.js') {
-        requireCache.alias[a + '.js'] = orig
-      }
-    })
+  // workspace aliases
+  alias.forEach(({ name, path }) => {
+    requireCache.alias[name] = path
   })
   console.log('init alias', alias, 'bundles',bundles)
   userInstances = params.userInstances
@@ -73,13 +68,13 @@ const importReg = /import(?:(?:(?:[ \n\t]+([^ *\n\t\{\},]+)[ \n\t]*(?:,|[ \n\t]+
 const exportReg = /export.*from/
 
 const runScript = async ({ script, url, base=globalBase, root=base }) => {
-  if(!script) script = readFileWeb(resolveUrl(url, base, root).url,{base})
+  if(!script) script = readFileWeb(resolveUrl(url, base, root).url)
 
   console.log('{ script, url, base, root }', { script, url, base, root })
   const shouldTransform = url.endsWith('.ts') || script.includes('import') && (importReg.test(script) || exportReg.test(script))
   let def = []
 
-  const scriptModule = require({url,script}, shouldTransform ? transformFunc : undefined, readFileWeb, base, root, readFileWeb)
+  const scriptModule = require({url,script}, shouldTransform ? transformFunc : undefined, readFileWeb, base, root)
   const fromSource = getParameterDefinitionsFromSource(script)
   def = combineParameterDefinitions(fromSource, await scriptModule.getParameterDefinitions?.())
   main = scriptModule.main

apps/engine-test/main.js
@@ -126,7 +126,7 @@ document.body.ondrop = async ev => {
     sendCmd('clearTempCache', {})
     const { alias, script } = await fileDropped(sw, files)
     projectName = sw.projectName
-    if (alias?.length) {
+    if (alias.length) {
       sendNotify('init', { alias })
     }
     runScript({ url: sw.fileToRun, base: sw.base })

packages/require/test/resolveUrl.test.js
@@ -5,11 +5,13 @@ import { resolveUrl } from '../src/resolveUrl.js'
 const localBase = 'http://localhost:5120/swfs/5810d898-ed92-4d48-b22f-1b7d6353117a/'
 const remoteBase = 'https://cdn.jsdelivr.net/npm/@jscad/io-utils/'
 const root = 'http://localhost:5120/swfs/5810d898-ed92-4d48-b22f-1b7d6353117a/'
+const fsRoot = 'fs:/'
 
 it('local file', () => {
   const expected = 'http://localhost:5120/swfs/5810d898-ed92-4d48-b22f-1b7d6353117a/imported.js'
   expect(resolveUrl('./imported.js', '/index.js', root).url).toEqual(expected)
   expect(resolveUrl('./imported.js', localBase, root).url).toEqual(expected)
+  expect(resolveUrl('./src/util.js', '/util/index.js', fsRoot).url).toEqual('fs:/util/src/util.js')
 })
 
 it('npm package', () => {
@@ -18,10 +20,16 @@ it('npm package', () => {
 })
 
 it('npm package internal file', () => {
-  expect(resolveUrl('@jscad/io-utils/convertToBlob', localBase, root).url).toEqual('https://cdn.jsdelivr.net/npm/@jscad/io-utils/convertToBlob.js')
+  expect(resolveUrl('@jscad/io-utils/convertToBlob', localBase, root).url)
+    .toEqual('https://cdn.jsdelivr.net/npm/@jscad/io-utils/convertToBlob.js')
 })
 
 it('npm package relative', () => {
   expect(resolveUrl('./convertToBlob.js', remoteBase, root).url)
     .toEqual('https://cdn.jsdelivr.net/npm/@jscad/io-utils/convertToBlob.js')
+})
+
+it('http url', () => {
+  expect(resolveUrl('http://localhost:5120/build/bundle.jscad_modeling.js', localBase, root).url)
+    .toEqual('http://localhost:5120/build/bundle.jscad_modeling.js')
 })

packages/require/test/workspace.import.test.js
@@ -1,7 +1,7 @@
 import { transformcjs } from '@jscadui/transform-babel'
 import { expect, it } from 'vitest'
 
-import { makeReadFileNode, readFileNode } from '../src/readFileNode.js'
+import { makeReadFileNode } from '../src/readFileNode.js'
 import { require, requireCache } from '../src/require.js'
 
 const root = 'test/workspace/import/'
@@ -10,13 +10,11 @@ const base = 'fs:/'
 it('no_transform', () => {
   const readFileNode = makeReadFileNode(root)
   let pack = JSON.parse(readFileNode('/package.json'))
-  // console.log('pack', pack)
   pack.workspaces?.forEach(w => {
     // todo move to utility
     let pack = JSON.parse(readFileNode(`/${w}/package.json`))
     let name = pack?.name || w
     let main = pack?.main || 'index.js'
-    console.log('pack', pack)
     requireCache.alias[name] = new URL(`./${w}/${main}`, base).toString()
   })
   let script = require('/index.js', transformcjs, readFileNode, base)

packages/require/test/workspace.import2level.test.js
@@ -1,7 +1,7 @@
 import { transformcjs } from '@jscadui/transform-babel'
 import { expect, it } from 'vitest'
 
-import { makeReadFileNode, readFileNode } from '../src/readFileNode.js'
+import { makeReadFileNode } from '../src/readFileNode.js'
 import { require, requireCache } from '../src/require.js'
 
 const root = 'test/workspace/import2level/'
@@ -10,15 +10,13 @@ const base = 'fs:/'
 it('no_transform', () => {
   const readFileNode = makeReadFileNode(root)
   let pack = JSON.parse(readFileNode('/package.json'))
-  // console.log('pack', pack)
   pack.workspaces?.forEach(w => {
     // todo move to utility
     let pack = JSON.parse(readFileNode(`/${w}/package.json`))
     let name = pack?.name || w
     let main = pack?.main || 'index.js'
-    console.log('pack', pack)
     requireCache.alias[name] = new URL(`./${w}/${main}`, base).toString()
   })
-  let script = require('/index.js', transformcjs, readFileNode, base)
+  let script = require('/index.js', transformcjs, readFileNode, base, base)
   expect(script.main({ size: 33 })).toEqual('cube33')
 })
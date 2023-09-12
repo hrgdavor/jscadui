082fc8231a6033650f7dc7d3c74c863cc4b0bc86
Kenny Daniel <platypii@gmail.com>
2023-09-12 07:03:18 -0700

    jscad-web: option to dismiss welcome screen

--- apps/jscad-web/src/welcome.js
+++ apps/jscad-web/src/welcome.js
@@ -2,20 +2,26 @@ const welcome = document.getElementById("welcome")
 let showing = true
 
 export const init = () => {
+  if (!welcome) return
   // hide the welcome menu when anything is clicked
   window.addEventListener("mousedown", (e) => dismiss(e))
   window.addEventListener("click", (e) => dismiss(e))
   window.addEventListener("drop", () => dismiss())
   window.addEventListener("dragstart", () => dismiss())
   window.addEventListener("dragover", () => dismiss())
+  document.getElementById('welcome-dismiss').addEventListener('click', () => {
+    localStorage.setItem('welcome.dismissed', true)
+    dismiss()
+  })
   // hello devs
   console.log('Welcome to JSCAD! Like JavaScript and want to help? Join us at https://github.com/jscad/OpenJSCAD.org')
 }
 
 export const dismiss = (e) => {
   // dismiss if click on anything other than a link
-  if (showing && (!e || !welcome.contains(e.target) || e.target.nodeName !== "A")) {
-    welcome.style.display = "none"
+  const isClickable = ['A', 'LABEL', 'INPUT'].includes(e?.target?.nodeName)
+  if (showing && (!e || !welcome.contains(e.target) || !isClickable)) {
+    welcome.remove()
     showing = false
   }
 }

acdc191988c2f3dd3097e2e55df371f682d08f4c
Davor Hrg <hrgdavor@gmail.com>
2023-12-29 10:05:52 +0100

    add missing @jscadui/orbit

--- apps/jscad-web/static/test.file.js
+++ apps/jscad-web/static/test.file.js
@@ -3,6 +3,8 @@ import * as jscad from '@jscad/modeling'
 export const main =({// @jscad-params
   mesh,// {type:"file"}
   check=false,
+  bla='test',
+  bla1='test', // {type:'choice', values:["a","b"]}
 })=>{  
   return mesh ? mesh : jscad.primitives.sphere()
 

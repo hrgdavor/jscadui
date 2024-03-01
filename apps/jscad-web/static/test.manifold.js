e88f2b3162bf02096d2d5d057e2fb0e69ace475e
Davor Hrg <hrgdavor@gmail.com>
2024-03-02 00:59:30 +0100

    tweak sliders

--- apps/jscad-web/static/test.manifold.js
+++ apps/jscad-web/static/test.manifold.js
@@ -3,13 +3,17 @@
 const jscad = require('@jscad/modeling')
 let ManifoldModule = require('manifold-3d')
 
-function toMesh(manifold){
+function toMesh(manifold) {
   let manifoldMesh = manifold.getMesh()
   return { type: 'mesh', vertices: manifoldMesh.vertProperties, indices: manifoldMesh.triVerts }
 }
 
 let Manifold
-async function main() {
+async function main({
+  // @jscad-params
+  segments = 32, // {type:'slider', min:10, max:64, live:true}
+  radius = 60, // {type:'slider', min:51, max:80, live:true}
+}) {
   if (ManifoldModule.cached) {
     Manifold = ManifoldModule.cached
   } else {
@@ -18,14 +22,19 @@ async function main() {
     Manifold.setup()
     ManifoldModule.cached = Manifold
   }
-  const {cube, sphere} = Manifold.Manifold
+  const { cube, sphere } = Manifold.Manifold
 
   //
-  const box = cube([100, 100, 100], true);
-  const ball = sphere(60, 100);
-  let result = box.subtract(ball);
-  result = toMesh(result)
-  return [result]
+  const box = cube([100, 100, 100], true)
+  const ball = sphere(radius, segments)
+  let result = box.subtract(ball)
+  try {
+    return [toMesh(result)]
+  } finally {
+    box.delete()
+    ball.delete()
+    result.delete()
+  }
   return [jscad.primitives.sphere({ size: 1 })]
 }
 

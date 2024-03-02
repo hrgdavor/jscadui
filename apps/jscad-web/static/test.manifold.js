a6719a93a8ef81a58f96ce25e95b9f86f17bca3c
Davor Hrg <hrgdavor@gmail.com>
2024-03-02 09:59:02 +0100

    more manifold exploration

--- apps/jscad-web/static/test.manifold.js
+++ apps/jscad-web/static/test.manifold.js
@@ -1,31 +1,35 @@
-// import * as jscad from '@jscad/modeling'
+import * as jscad from '@jscad/modeling'
+// import * as ManifoldModule from 'manifold-3d'
 
-const jscad = require('@jscad/modeling')
+//const jscad = require('@jscad/modeling')
 let ManifoldModule = require('manifold-3d')
 
 function toMesh(manifold) {
   let manifoldMesh = manifold.getMesh()
   return { type: 'mesh', vertices: manifoldMesh.vertProperties, indices: manifoldMesh.triVerts }
 }
+console.log('ManifoldModule',ManifoldModule)
+if(!ManifoldModule.get){
+  ManifoldModule.get = async function(){
+    if (!ManifoldModule.cached) {
+      let Manifold
+      Manifold = await ManifoldModule()
+      Manifold.setup()
+      ManifoldModule.cached = Manifold
+    }
+    return ManifoldModule.cached.Manifold
+  }
+}
 
-let Manifold
-async function main({
+export async function main({
   // @jscad-params
   useManifold = true,
   segments = 32, // {type:'slider', min:10, max:64, live:true}
   radius = 60, //16 {type:'slider', min:51, max:80, live:true}
 }) {
-  if (ManifoldModule.cached) {
-    Manifold = ManifoldModule.cached
-  } else {
-    console.log()
-    Manifold = await ManifoldModule()
-    Manifold.setup()
-    ManifoldModule.cached = Manifold
-  }
-  const { cube, sphere } = Manifold.Manifold
+  const { cube, sphere } = await ManifoldModule.get()
 
-  if(useManifold){
+  if (useManifold) {
     const box = cube([100, 100, 100], true)
     const ball = sphere(radius, segments)
     let result = box.subtract(ball)
@@ -36,12 +40,11 @@ async function main({
       ball.delete()
       result.delete()
     }
-  }else{
-    const box = jscad.primitives.cube({size:100})
+  } else {
+    const box = jscad.primitives.cube({ size: 100 })
     const ball = jscad.primitives.sphere({ radius, segments })
-    return jscad.booleans.subtract(box,ball)
+    return jscad.booleans.subtract(box, ball)
   }
-
 }
 
-module.exports = { main }
+
--- packages/require/README.md
+++ packages/require/README.md
@@ -1,1 +1,6 @@
 # require
+This is an implementation of import/require to be able to run javascript(and typescript) files.
+
+It is used in jscad.app to execute the scripts
+
+# 

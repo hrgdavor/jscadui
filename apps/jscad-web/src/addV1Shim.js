ce022ebd9b3c94e5f5f8161b6880beaac9ddaae9
Davor Hrg <hrgdavor@gmail.com>
2024-03-20 15:06:51 +0100

    v1 shim

--- apps/jscad-web/src/addV1Shim.js
+++ apps/jscad-web/src/addV1Shim.js
@@ -1,7 +1,8 @@
-export function addV1Shim(script){
-  if(script.includes('JS V1 SHIM HEADER')) return script
+export function addV1Shim(script) {
+  if (script.includes('JS V1 SHIM HEADER')) return script
 
   return `const csg = require('@jscad/csg')
+const { OpenJsCad, debug } = csg
 const {circle, square, polygon, triangle} = csg.primitives2d
 const {cube, sphere, cylinder, geodesicSphere, torus, polyhedron} = csg.primitives3d
 const {union, difference, intersection} = csg.booleanOps
@@ -9,11 +10,13 @@ const {translate, center, scale, rotate, transform, mirror, expand, contract, mi
 const {extrudeInOrthonormalBasis, extrudeInPlane, extrude, linear_extrude, rotate_extrude, rotateExtrude, rectangular_extrude} = csg.extrusions
 const {css2rgb, color, rgb2hsl, hsl2rgb, rgb2hsv, hsv2rgb, html2rgb, rgb2html} = csg.color
 const {sin, cos, asin, acos, tan, atan, atan2, ceil, floor, abs, min, max, rands, log, lookup, pow, sign, sqrt, round} = csg.maths
-const {vector_char, vector_text, vectorChar, vectorText} = csg.text
+const {vector_char, vector_text, vectorText} = csg.text
+const { CSG, CAG } = csg.csg
+function echo(){console.warn("echo() will be deprecated in the near future: please use console.log/warn/error instead");for(var e="",t=arguments,n=0;n<t.length;n++)n&&(e+=", "),e+=t[n];console.log(e)}
 // lines above are a JS shim, to make .jscad scripts work as regular JS
 // ------------------------------ JS V1 SHIM HEADER ---------------------------------------------------------------------------------------
   
-  ${script}
+${script}
 
 
 // ---------------------------- JS V1 SHIM FOOTER -----------------------------------------------------------------------------------------

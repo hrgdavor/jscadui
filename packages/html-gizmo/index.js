3127f2d27a97881d0ef1a79135ac4f129c86017e
Robin Harbecke <robin.harbecke@gmail.com>
2024-11-26 00:02:06 +0100

    Move onClick to makeSide

--- packages/html-gizmo/index.js
+++ packages/html-gizmo/index.js
@@ -22,36 +22,7 @@ export const names = {
 }
 
 /**
- * @param {SideNames} names 
- * @param {string} name 
- * @param  {...string} parts 
- * @returns {HTMLDivElement}
- */
-const makeSide = (names, name, ...parts) => {
-  const result = document.createElement('div')
-  result.setAttribute('part', 'face')
-  result.classList.add('cube__face', `cube__face--${name}`)
-
-  const bg = document.createElement('div')
-  bg.classList.add('bg')
-  bg.setAttribute('part', 'face-bg')
-
-  result.append(bg)
-
-  result.append(
-    ...parts.flatMap(part => part.split(',').map(c => {
-      const i = document.createElement('i')
-      i.setAttribute('c', c)
-      i.textContent = names[c] ?? ''
-      return i
-    }))
-  )
-
-  return result
-}
-
-/** 
- * If using in HTML as tag <jscadui-gizmo/> you must call the static method Gizmo.define().
+  * If using in HTML as tag <jscadui-gizmo/> you must call the static method Gizmo.define().
  * 
  * If creating from code via new Gizmo() static initializer will be triggered automatically.
  * 
@@ -93,10 +64,6 @@ export class Gizmo extends HTMLElement {
 
     this.setNames(this.names)
 
-    first.addEventListener('click', (e) => {
-      const cam = e.target.getAttribute('c')
-      if (cam) this.oncam?.(cam)
-    })
 
     const mouseover = (el, over) => {
       const cam = el.getAttribute('c')
@@ -118,12 +85,12 @@ export class Gizmo extends HTMLElement {
 
   setNames(_names = names) {
     this.#first.append(
-      makeSide(_names, 'T', 'TNW,TN,TNE', 'TW,T,TE', 'TSW,TS,TSE'),
-      makeSide(_names, 'B', 'BSW,BS,BSE', 'BW,B,BE', 'BNW,BN,BNE'),
-      makeSide(_names, 'S', 'TSW,TS,TSE', 'SW,S,SE', 'BSW,BS,BSE'),
-      makeSide(_names, 'N', 'TNE,TN,TNW', 'NE,N,NW', 'BNE,BN,BNW'),
-      makeSide(_names, 'E', 'TSE,TE,TNE', 'SE,E,NE', 'BSE,BE,BNE'),
-      makeSide(_names, 'W', 'TNW,TW,TSW', 'NW,W,SW', 'BNW,BW,BSW'),
+this.#makeSide(_names, 'T', 'TNW,TN,TNE', 'TW,T,TE', 'TSW,TS,TSE'),
+this.#makeSide(_names, 'B', 'BSW,BS,BSE', 'BW,B,BE', 'BNW,BN,BNE'),
+this.#makeSide(_names, 'S', 'TSW,TS,TSE', 'SW,S,SE', 'BSW,BS,BSE'),
+this.#makeSide(_names, 'N', 'TNE,TN,TNW', 'NE,N,NW', 'BNE,BN,BNW'),
+this.#makeSide(_names, 'E', 'TSE,TE,TNE', 'SE,E,NE', 'BSE,BE,BNE'),
+this.#makeSide(_names, 'W', 'TNW,TW,TSW', 'NW,W,SW', 'BNW,BW,BSW'),
     )
   }
 
@@ -142,5 +109,39 @@ export class Gizmo extends HTMLElement {
     if (typeof rx === 'number') rx = rx + 'rad'
     if (typeof rz === 'number') rz = rz + 'rad'
     this.style.setProperty('--cube-transform', `scale3d(0.8,0.8,0.8) rotateX(${rx}) rotateZ(${rz})`)
+  }
+
+  /**
+   * @param {SideNames} names 
+   * @param {string} name 
+   * @param  {...string} parts 
+   * @returns {HTMLDivElement}
+   */
+  #makeSide = (names, name, ...parts) => {
+    const result = document.createElement('div')
+    result.setAttribute('part', 'face')
+    result.classList.add('cube__face', `cube__face--${name}`)
+
+    const bg = document.createElement('div')
+    bg.classList.add('bg')
+    bg.setAttribute('part', 'face-bg')
+
+    result.append(bg)
+
+    result.append(
+      ...parts.flatMap(part => part.split(',').map(c => {
+        const i = document.createElement('i')
+        i.setAttribute('c', c)
+        i.textContent = names[c] ?? ''
+        i.addEventListener('click', e => {
+          e.preventDefault()
+          e.stopPropagation()
+          this.oncam?.(c)
+        })
+        return i
+      }))
+    )
+
+    return result
   }
 }

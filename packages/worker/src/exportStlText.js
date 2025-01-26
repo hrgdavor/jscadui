c4c78135a95377ba1384c607869305640287a6c7
Robin Harbecke <robin.harbecke@gmail.com>
2025-01-26 15:19:45 +0100

    Fix function name

--- packages/worker/src/exportStlText.js
+++ packages/worker/src/exportStlText.js
@@ -12,17 +12,17 @@ const convertToStl = (objects, out) => {
   return out
 }
 
-const vertextoStlString = (prefix, v, idx) => `${prefix} ${v[idx]} ${v[idx+1]} ${v[idx+2]}\n`
+const vertexToStlString = (prefix, v, idx) => `${prefix} ${v[idx]} ${v[idx + 1]} ${v[idx + 2]}\n`
 
 const convertToFacets = (polygon, out) => {
   const {vertices, indices, normals} = polygon
   const max = indices.length - 2
   for(let i=0; i<max; i+=3){
-    out.push(vertextoStlString('facet normal',normals,i))
+    out.push(vertexToStlString('facet normal', normals, i))
     out.push('outer loop\n')
-    out.push(vertextoStlString('vertex',vertices, indices[i]*3))  
-    out.push(vertextoStlString('vertex',vertices, indices[i+1]*3))  
-    out.push(vertextoStlString('vertex',vertices, indices[i+2]*3))  
+    out.push(vertexToStlString('vertex', vertices, indices[i] * 3))
+    out.push(vertexToStlString('vertex', vertices, indices[i + 1] * 3))
+    out.push(vertexToStlString('vertex', vertices, indices[i + 2] * 3))  
     out.push('endloop\nendfacet\n')
   }
   return out

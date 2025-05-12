1e1000ca8fbc91fbf8085936e5866e5d787f6119
RJ Salvador <salvador.workshop@gmail.com>
2025-05-12 10:38:04 -0400

    WIP - First real builder content

--- apps/jscad-web/lib-demo/builders-v2/roof.js
+++ apps/jscad-web/lib-demo/builders-v2/roof.js
@@ -1,5 +1,28 @@
 
 const roofBuilder = ({ lib, swLib }) => {
+    const { triangle } = lib.primitives;
+    const { rotate } = lib.transforms;
+    const { extrudeLinear } = lib.extrusions;
+
+    const getBasicSpecs = ({ roofSpanSize, roofPitch }) => {
+        const roofHeightX = Math.tan(roofPitch) * roofSpanSize[0];
+        const roofHypotX = Math.hypot(roofSpanSize[0], roofHeightX);
+
+        const roofHeightY = Math.tan(roofPitch) * roofSpanSize[1];
+        const roofHypotY = Math.hypot(roofSpanSize[1], roofHeightY);
+
+        return {
+            x: {
+                height: roofHeightX,
+                hypot: roofHypotX,
+            },
+            y: {
+                height: roofHeightY,
+                hypot: roofHypotY,
+            }
+        }
+    }
+
     /**
      * Builds a gable roof
      * @param {Object} opts 
@@ -44,6 +67,7 @@ const roofBuilder = ({ lib, swLib }) => {
      * @param {number[]} opts.roofSpanSize - [x,y] size of area to be spanned
      * @param {number[]} opts.roofOverhangSize - [x,y] size for overhangs
      * @param {number} opts.roofPitch - Roof pitch angle, expressed in radians
+     * @param {string} opts.roofAxis - Main axis of roof
      * @param {string[]} opts.roofOpts - Options like 'solid', 'noWall'
      * @param {number} opts.wallThickness - Wall thickness
      * @returns Shed roof
@@ -52,10 +76,32 @@ const roofBuilder = ({ lib, swLib }) => {
         roofSpanSize,
         roofOverhangSize,
         roofPitch,
+        roofAxis = 'x',
         roofOpts = ['solid'],
         wallThickness,
     }) => {
-        return null;
+        console.log(`buildShedRoof() roofSpanSize = ${JSON.stringify(roofSpanSize)}`);
+        const basicSpecs = getBasicSpecs({ roofPitch, roofSpanSize })
+        const mainAxisIdx = roofAxis === 'x' ? 0 : 1;
+        const otherAxisIdx = mainAxisIdx === 0 ? 1 : 0;
+        console.log(`    roofOpts = ${JSON.stringify(roofOpts)}`);
+        console.log(`    basicSpecs = ${JSON.stringify(basicSpecs)}`);
+        console.log(`    mainAxisIdx = ${JSON.stringify(mainAxisIdx)}, otherAxisIdx = ${JSON.stringify(otherAxisIdx)}`);
+
+        const roofSpan = roofSpanSize[mainAxisIdx];
+        const roofHeight = basicSpecs[roofAxis].height;
+        const roofHypot = basicSpecs[roofAxis].hypot;
+
+        const offHeight = basicSpecs[roofAxis].height;
+        const offHypot = basicSpecs[roofAxis].hypot;
+
+        const baseTriangle = triangle({ type: 'SAS', values: [roofSpan, Math.PI / 2, roofHeight] });
+        const basePrism = rotate(
+            [Math.PI / 2, 0, 0],
+            extrudeLinear({ height: roofSpanSize[otherAxisIdx] }, baseTriangle)
+        );
+
+        return basePrism;
     }
 
     return {
--- apps/jscad-web/lib-demo/roof-examples.js
+++ apps/jscad-web/lib-demo/roof-examples.js
@@ -2,10 +2,11 @@
 
 const jscad = require('@jscad/modeling')
 const swJscad = require('sw-jscad').init({ lib: jscad });
+require('./enhancer').enhanceSwJscad({ lib: jscad, swLib: swJscad });
 
 const { translate } = jscad.transforms;
 const {
-    entryways,
+    roofs,
     layout,
 } = swJscad
 
@@ -15,25 +16,20 @@ const main = () => {
         noFrame: true,
     }
 
-    // const entryway1 = entryways.buildGothic({
-    //     wallLength: 100,
-    //     wallThickness: 10,
-    //     wallHeight: 100,
-    //     entryLength: 40,
-    //     trimOpts: ['base', 'dado', 'crown'],
-    //     crownUnits: 2,
-    //     dadoUnits: 2,
-    //     baseUnits: 1,
-    //     trimUnitHeight: 4,
-    //     trimUnitDepth: 1.25,
-    // });
-    // layout.addToLayout({ name: 'entryway1', desc: '...', geom: entryway1, layoutOpts });
+    const defaultRoofOpts = {
+        roofSpanSize: [30, 50],
+        roofPitch: Math.PI / 5,
+        wallThickness: 10,
+    }
+
+    const roof1 = roofs.buildShedRoof({
+        ...defaultRoofOpts
+    });
+    layout.addToLayout({ name: 'roof1', desc: '...', geom: roof1, layoutOpts });
 
     const layoutContent = layout.gridLayout({ layoutOpts });
 
-    // return layoutContent
-
-    return []
+    return layoutContent
 }
 
 module.exports = { main }

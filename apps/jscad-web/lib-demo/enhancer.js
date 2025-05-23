be81ce0ba6d283f2e1e11ecd4086a79dade35e9f
RJ Salvador <salvador.workshop@gmail.com>
2025-05-23 09:43:45 -0400

    WIP - started fixing roof issues

--- apps/jscad-web/lib-demo/enhancer.js
+++ apps/jscad-web/lib-demo/enhancer.js
@@ -5,6 +5,7 @@
 //-------------------
 
 const buttressBuilder = require('./builders-v2/buttress');
+const roofsBuilder = require('./builders-v2/roofs');
 
 
 //------------------
@@ -13,6 +14,7 @@ const buttressBuilder = require('./builders-v2/buttress');
 
 const enhanceSwJscad = ({ lib, swLib }) => {
     swLib.buttress = buttressBuilder.init({ lib, swLib });
+    swLib.roofs = roofsBuilder.init({ lib, swLib });
 }
 
 module.exports = {

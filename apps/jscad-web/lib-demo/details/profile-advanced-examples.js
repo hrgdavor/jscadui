979766b4a78cc7c8703c414ff6c8eb778b314055
RJ Salvador <salvador.workshop@gmail.com>
2025-07-08 20:39:27 -0400

    added more advanced profiles

--- apps/jscad-web/lib-demo/details/profile-advanced-examples.js
+++ apps/jscad-web/lib-demo/details/profile-advanced-examples.js
@@ -16,15 +16,43 @@ const main = () => {
     const reinforcementProfiles = [
         {
             name: 're-straight',
-            geom: profiles.reinforcement.straight({ length: 24, thickness: 4, offsetWidths: [4] }),
+            geom: profiles.reinforcement.straight({ length: 32, thickness: 4, insetWidth: 4, offsetWidth: 6 }),
+        },
+        {
+            name: 're-corner',
+            geom: profiles.reinforcement.corner({ length: 48, depth: 32, thickness: 4, flangeThickness: 1, insetWidth: 4, offsetWidth: 6 }),
+        },
+        {
+            name: 're-cBeam',
+            geom: profiles.reinforcement.cBeam({ length: 48, depth: 32, thickness: 4, flangeThickness: 1, insetWidth: 4, offsetWidth: 6 }),
+        },
+        {
+            name: 're-tBeam',
+            geom: profiles.reinforcement.tBeam({ length: 48, depth: 32, thickness: 4, flangeThickness: 1, insetWidth: 4, offsetWidth: 6 }),
+        },
+        {
+            name: 're-doubleTBeam',
+            geom: profiles.reinforcement.doubleTBeam({ length: 48, depth: 32, thickness: 4, insetWidth: 4, offsetWidth: 6 }),
+        },
+        {
+            name: 're-triBeam',
+            geom: profiles.reinforcement.triBeam({ radius: 48, thickness: 3, insetWidth: 3, offsetWidth: 6 }),
+        },
+        {
+            name: 're-crossBeam',
+            geom: profiles.reinforcement.crossBeam({ radius: 48, thickness: 3, insetWidth: 3, offsetWidth: 6 }),
+        },
+        {
+            name: 're-hexBeam',
+            geom: profiles.reinforcement.hexBeam({ radius: 48, thickness: 3, insetWidth: 3, offsetWidth: 6 }),
         },
     ]
 
     const connectionProfiles = [
-        {
-            name: 'conn-pegboard',
-            geom: null,
-        },
+        // {
+        //     name: 'conn-pegboard',
+        //     geom: null,
+        // },
     ]
 
     const profiles2ndGen = [...reinforcementProfiles, ...connectionProfiles]

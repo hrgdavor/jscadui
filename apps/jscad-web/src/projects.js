ed00147b2a3f445e37be3383728b24af0897a400
RJ Salvador <salvador.workshop@gmail.com>
2025-07-11 21:11:53 -0400

    added link to new project

--- apps/jscad-web/src/projects.js
+++ apps/jscad-web/src/projects.js
@@ -58,4 +58,5 @@ export const swModels = [
 export const swProjects = [
   { name: 'Connectors', source: './projects/connectors/index.js' },
   { name: 'Connector Parts', source: './projects/connectors/connector-parts/index.js' },
+  { name: 'Shelf 001', source: './projects/shelf-001/index.js' },
 ]
